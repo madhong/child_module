f2d2380f6f45d16f343adde08d78a9638f3a2adb
liuyihong.ext <liuyihong.ext@geniusafc.com>
2022-01-24 09:24:54 +0800

    添加菜单

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <!-- <img alt="Vue logo" src="./assets/logo.png" /> -->
-  <hr />
+  <!-- <hr /> -->
   <div class="color">子模块</div>
   <HelloWorld msg="Welcome to Your Vue.js App" />
   <ccc></ccc>
@@ -24,7 +24,7 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  /* margin-top: 60px; */
 }
 .color {
   color: blue;
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,63 +1,66 @@
 <template>
-  <div class="hello">
-    <h3>Welcome to child Module Vue.js App</h3>
-    <ul>
-      <li>
-        <a href="https://router.vuejs.org" target="_blank" rel="noopener"
-          >vue-router</a
-        >
-      </li>
-      <li>
-        <a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-devtools#vue-devtools"
-          target="_blank"
-          rel="noopener"
-          >vue-devtools</a
-        >
-      </li>
-      <li>
-        <a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener"
-          >vue-loader</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-          rel="noopener"
-          >awesome-vue</a
-        >
-      </li>
-    </ul>
-  </div>
+  <a-menu
+    id="dddddd"
+    v-model:openKeys="openKeys"
+    v-model:selectedKeys="selectedKeys"
+    style="width: 256px"
+    mode="inline"
+    @click="handleClick"
+  >
+    <a-sub-menu key="sub1" @titleClick="titleClick">
+      <template #icon>
+        <MailOutlined />
+      </template>
+      <template #title>目录 one</template>
+      <a-menu-item-group key="g1">
+        <template #icon>
+          <QqOutlined />
+        </template>
+        <template #title>Item 1</template>
+        <a-menu-item key="1">菜单 1</a-menu-item>
+        <a-menu-item key="2">菜单 2</a-menu-item>
+      </a-menu-item-group>
+      <a-menu-item-group key="g2" title="Item 2">
+        <a-menu-item key="3">菜单 3</a-menu-item>
+        <a-menu-item key="4">菜单 4</a-menu-item>
+      </a-menu-item-group>
+    </a-sub-menu>
+  </a-menu>
 </template>
-
 <script>
-export default {
+import { defineComponent, ref, watch } from "vue";
+import { MailOutlined, QqOutlined } from "@ant-design/icons-vue";
+export default defineComponent({
   name: "HelloWorld",
-  props: {
-    msg: String,
+  components: {
+    MailOutlined,
+    QqOutlined,
   },
-};
-</script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
+  setup() {
+    const selectedKeys = ref(["1"]);
+    const openKeys = ref(["sub1"]);
+
+    const handleClick = (e) => {
+      console.log("click", e);
+    };
+
+    const titleClick = (e) => {
+      console.log("titleClick", e);
+    };
+
+    watch(
+      () => openKeys,
+      (val) => {
+        console.log("openKeys", val);
+      }
+    );
+    return {
+      selectedKeys,
+      openKeys,
+      handleClick,
+      titleClick,
+    };
+  },
+});
+</script>
