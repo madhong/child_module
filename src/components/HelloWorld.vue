defce2f5772871040003e3e22474b1435562a037
liuyihong.ext <liuyihong.ext@geniusafc.com>
2022-01-24 14:28:56 +0800

    更新菜单

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,66 +1,21 @@
 <template>
-  <a-menu
-    id="dddddd"
-    v-model:openKeys="openKeys"
-    v-model:selectedKeys="selectedKeys"
-    style="width: 256px"
-    mode="inline"
-    @click="handleClick"
-  >
-    <a-sub-menu key="sub1" @titleClick="titleClick">
-      <template #icon>
-        <MailOutlined />
-      </template>
-      <template #title>目录 one</template>
-      <a-menu-item-group key="g1">
-        <template #icon>
-          <QqOutlined />
-        </template>
-        <template #title>Item 1</template>
-        <a-menu-item key="1">菜单 1</a-menu-item>
-        <a-menu-item key="2">菜单 2</a-menu-item>
-      </a-menu-item-group>
-      <a-menu-item-group key="g2" title="Item 2">
-        <a-menu-item key="3">菜单 3</a-menu-item>
-        <a-menu-item key="4">菜单 4</a-menu-item>
-      </a-menu-item-group>
-    </a-sub-menu>
-  </a-menu>
+  <a-layout>
+    <a-layout-sider>
+      <mymenu></mymenu>
+    </a-layout-sider>
+    <a-layout>
+      <a-layout-header>Header</a-layout-header>
+      <a-layout-content>Content</a-layout-content>
+    </a-layout>
+  </a-layout>
 </template>
 <script>
-import { defineComponent, ref, watch } from "vue";
-import { MailOutlined, QqOutlined } from "@ant-design/icons-vue";
+import { defineComponent } from "vue";
+import mymenu from "./menu.vue";
 export default defineComponent({
   name: "HelloWorld",
   components: {
-    MailOutlined,
-    QqOutlined,
-  },
-
-  setup() {
-    const selectedKeys = ref(["1"]);
-    const openKeys = ref(["sub1"]);
-
-    const handleClick = (e) => {
-      console.log("click", e);
-    };
-
-    const titleClick = (e) => {
-      console.log("titleClick", e);
-    };
-
-    watch(
-      () => openKeys,
-      (val) => {
-        console.log("openKeys", val);
-      }
-    );
-    return {
-      selectedKeys,
-      openKeys,
-      handleClick,
-      titleClick,
-    };
+    mymenu,
   },
 });
 </script>
---
+++ src/components/menu.vue
@@ -0,0 +1,55 @@
+<template>
+  <a-menu
+    id="dddddd"
+    v-model:openKeys="openKeys"
+    v-model:selectedKeys="selectedKeys"
+    style="width: 100%; background-color: rgb(165, 161, 161)"
+    mode="inline"
+    @click="handleClick"
+  >
+    <a-sub-menu key="sub1" @titleClick="titleClick">
+      <template #icon>
+        <MailOutlined />
+      </template>
+      <template #title>子项目 one</template>
+      <a-menu-item key="1">菜单 1</a-menu-item>
+      <a-menu-item key="2">菜单 2</a-menu-item>
+    </a-sub-menu>
+  </a-menu>
+</template>
+<script>
+import { defineComponent, ref, watch } from "vue";
+import { MailOutlined } from "@ant-design/icons-vue";
+export default defineComponent({
+  name: "menu",
+  components: {
+    MailOutlined,
+  },
+
+  setup() {
+    const selectedKeys = ref([""]);
+    const openKeys = ref(["sub1"]);
+
+    const handleClick = (e) => {
+      console.log("click", e);
+    };
+
+    const titleClick = (e) => {
+      console.log("titleClick", e);
+    };
+
+    watch(
+      () => openKeys,
+      (val) => {
+        console.log("openKeys", val);
+      }
+    );
+    return {
+      selectedKeys,
+      openKeys,
+      handleClick,
+      titleClick,
+    };
+  },
+});
+</script>
